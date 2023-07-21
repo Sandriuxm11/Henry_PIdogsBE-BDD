8786d1d30c6924a0565862c0b74d687bae2d4b06
Sandriuxm11 <117607814+Sandriuxm11@users.noreply.github.com>
2023-07-20 22:37:22 -0500

    first commit

---
+++ src/models/Dog.js
@@ -0,0 +1,34 @@
+const { DataTypes, UUIDV4 } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('Dog', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: UUIDV4,
+      primaryKey: true,
+    },
+    imagen: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      unique: true, 
+      allowNull: false,
+    },
+    altura: {
+      type: DataTypes.DECIMAL,
+    },
+    peso: {
+      type: DataTypes.DECIMAL,
+    },
+    años_de_vida: {
+      type: DataTypes.DECIMAL,
+    },
+  },{
+    timestamps: false,
+  });
+};
