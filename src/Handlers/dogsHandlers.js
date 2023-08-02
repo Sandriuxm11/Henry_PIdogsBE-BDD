d3d2eecf44bf2686e0fd6999d65557638479e768
Sandriuxm11 <117607814+Sandriuxm11@users.noreply.github.com>
2023-08-01 22:58:54 -0500

    Terminado GetPerro por nombre

--- src/Handlers/dogsHandlers.js
+++ src/Handlers/dogsHandlers.js
@@ -4,12 +4,6 @@ const getRazaByName = require("../controllers/02-getRazaByName");
 const createNewDog = require("../controllers/04-createNewDog");
 
 const dogsHandler = async (req, res) => {
-   
-    // Esta ruta debe obtener todas aquellas razas de perros que coinciden con el nombre recibido por query. (No es necesario que sea una coincidencia exacta).
-    // Debe poder buscarlo independientemente de mayúsculas o minúsculas.
-    // Si no existe la raza, debe mostrar un mensaje adecuado.
-    // Debe buscar tanto los de la API como los de la base de datos.
-    
     const {name} = req.query;
 
         try {
--- src/controllers/02-getRazaByName.js
+++ src/controllers/02-getRazaByName.js
@@ -1,26 +1,12 @@
-const {Dog} = require("../db");
-const axios = require("axios");
-const {API_KEY} = process.env;
-const URL = `https://api.thedogapi.com/v1/breeds?${API_KEY}`;
-const arrayFiltrada = require("../Filtros/filtroarray");
+const getAllDogs = require ("./01-getAlldogs");
 
 const getRazaByName = async (name) => {
-    // Pasar todo a LowerCase - Debe poder buscarlo independientemente de mayúsculas o minúsculas.
-    // 
-    // const nameFinal = toLowerCase(name);
+    const allDogs = await getAllDogs();
 
-    const databaseDogs = await Dog.findAll({where: {name : name}});
+    const filteredName = allDogs.filter(dog => dog.name.toLowerCase().includes(name.toLowerCase()));
 
-    const apiDogsRaw = (await axios.get(URL)).data;
-    const apiDogs = arrayFiltrada(apiDogsRaw);
-    const nombreBuscado = apiDogs.filter(dog => {
-        return dog.name === name;
-    })
-
-    const results = [...databaseDogs, ...nombreBuscado];
-    
-    if (!results.length) throw Error ("NO EXISTE LA RAZA DE PERRO BUSCADA");
-    else return results; 
+    if (!filteredName.length) throw Error ("NO EXISTE LA RAZA DE PERRO BUSCADA");
+    else return filteredName;
 };
 
 module.exports = getRazaByName;
