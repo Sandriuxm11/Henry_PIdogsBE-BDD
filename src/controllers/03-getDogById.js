81f995be0ffd603fcd88b1ac9c6baf8e2ac1f2ea
Sandriuxm11 <117607814+Sandriuxm11@users.noreply.github.com>
2023-08-06 01:28:59 -0500

    BackEnd Terminado, puede mejorar

--- src/controllers/03-getDogById.js
+++ src/controllers/03-getDogById.js
@@ -1,17 +1,28 @@
-const {Dog} = require("../db");
+const {Dog, Temperament} = require("../db");
 const axios = require("axios");
 const {API_KEY} = process.env;
 const URL = `https://api.thedogapi.com/v1/breeds?${API_KEY}`;
 const arrayFiltrada = require("../Filtros/filtroarray");
+const filtroArrayBdd = require("../Filtros/filtroarraybdd");
 
 const getDogById = async (id, source) => {
     
     const allDogsRaw = (await axios.get(URL)).data;
-
     const allDogs = arrayFiltrada(allDogsRaw);
 
+    const allDogsBddRaw = await Dog.findAll({
+        include: {
+            model: Temperament,
+            attributes: ["name"],
+            through: {
+                attributes: [],
+            }
+        }
+    });
+    const bddDogs = await filtroArrayBdd(allDogsBddRaw);
+
     const dogId = source === "bdd"
-        ? await Dog.findByPk(id)
+        ? await bddDogs.find(dog => dog.id === id)
         : await allDogs.find(dog => dog.id === Number(id));
 
     if(!dogId) throw Error ("NO EXISTE EL ID BUSCADO");
